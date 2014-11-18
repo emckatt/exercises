ca0b75fb5c4be56335effa26d85559561715e0c1
Matthew Drake <mdrake@yahoo-inc.com>
2014-11-17 19:48:38 -0800

    added exercise 1

--- intro/e0-run-script.py
+++ intro/e0-run-script.py
@@ -2,7 +2,7 @@
 # Run the script by doing using IDLE or the command line
 # Add a comment by putting a pound sign and then typing what you want
 
-print " ".join(["Hello","Emerson", "good", "luck"])
+print(" ".join(["Hello","Emerson", "good", "luck"]))
 
 # add a comment with what gets printed by running this
 # 
---
+++ intro/e1-print-hw.py
@@ -0,0 +1,15 @@
+# Programming works using different types of data
+# These "types" are very important and suggest different behaviors
+
+# The one type you'll be using with this excercise is string
+# Strings are letters strung together (get it - string) to make words and phrases
+# When you use "print", you can give it a string and it will print it!
+# 
+# Example: print "hi there"
+#     print("hi there")
+#
+# Easy!
+# for this excercise, write a print statement with the string "hello world"
+
+# start right below:
+
